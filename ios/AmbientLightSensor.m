8cb0a7787fe9bf5e28dab942abf8441434ddc5f9
k-lpmg <kanglpmg@gmail.com>
2024-04-07 18:06:27 +0900

    feat: Add LightSensor event

--- ios/AmbientLightSensor.m
+++ ios/AmbientLightSensor.m
@@ -1,6 +1,7 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(AmbientLightSensor, NSObject)
+@interface RCT_EXTERN_MODULE(AmbientLightSensor, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(hasLightSensor: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(startLightSensor)
--- ios/AmbientLightSensor.swift
+++ ios/AmbientLightSensor.swift
@@ -2,19 +2,31 @@ import Foundation
 import UIKit
 import AVFoundation
 import SensorKit
+import React
 
 @objc(AmbientLightSensor)
-class AmbientLightSensor: NSObject {
+class AmbientLightSensor: RCTEventEmitter {
+    let queue = DispatchQueue(label: "AmbientLightSensor")
+    let eventName = "LightSensor"
+
+    override init() {
+        super.init()
+    }
+
+    override func supportedEvents() -> [String]! {
+        return ["LightSensor"]
+    }
     
     @objc func hasLightSensor(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
         resolve(true)
     }
     
-    
     @objc func startLightSensor() -> Void {
+        sendEvent(withName: eventName, body: ["lightValue": 100])
     }
     
     @objc func stopLightSensor() -> Void {
+        sendEvent(withName: eventName, body: ["lightValue": 0])
     }
     
 }
