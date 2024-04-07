f206ead07252a9360de799a1443d140cedc69c2f
k-lpmg <kanglpmg@gmail.com>
2024-04-07 17:08:45 +0900

    fix: typo

--- ios/AmbientLightSensor.swift
+++ ios/AmbientLightSensor.swift
@@ -1,9 +1,8 @@
 @objc(AmbientLightSensor)
 class AmbientLightSensor: NSObject {
-
-  @objc
-  func getGreeting(_ callback successCallback: 
-RCTResponseSenderBlock) { // Assume name comes from the any native API side
+    
+    @objc func getGreeting(_ callback successCallback:
+  RCTResponseSenderBlock) { // Assume name comes from the any native API side
     successCallback(["Hi, I'm iOS"])
   }
 }
