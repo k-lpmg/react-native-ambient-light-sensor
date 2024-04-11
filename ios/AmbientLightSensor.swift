bb4e5e73accb3d32873bef9442b674310ae2a9cc
k-lpmg <kanglpmg@gmail.com>
2024-04-11 18:48:14 +0900

    feat: Add setupCaptureSession

--- ios/AmbientLightSensor.swift
+++ ios/AmbientLightSensor.swift
@@ -4,12 +4,16 @@ import AVFoundation
 import React
 
 @objc(AmbientLightSensor)
-class AmbientLightSensor: RCTEventEmitter {
+class AmbientLightSensor: RCTEventEmitter, AVCaptureVideoDataOutputSampleBufferDelegate {
     let queue = DispatchQueue(label: "AmbientLightSensor")
     let eventName = "LightSensor"
 
+    private let captureSession = AVCaptureSession()
+    private let videoOutput = AVCaptureVideoDataOutput()
+
     override init() {
         super.init()
+
     }
 
     override func supportedEvents() -> [String]! {
@@ -21,13 +25,46 @@ class AmbientLightSensor: RCTEventEmitter {
     }
     
     @objc func startLightSensor() -> Void {
-        sendEvent(withName: eventName, body: ["lightValue": 100])
+        setupCaptureSession();
     }
     
     @objc func stopLightSensor() -> Void {
-        let stopLux = ambientLightSample.lux;
+        sendEvent(withName: eventName, body: ["lightValue": 0])
+    }
+
+    // Private methods
+
+    private func setupCaptureSession() {
+        guard let camera = AVCaptureDevice.default(for: .video) else { return }
         
-        sendEvent(withName: eventName, body: ["lightValue": 100])
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(cameraInput)
+            
+            captureSession.addOutput(videoOutput)
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            
+            captureSession.startRunning()
+        } catch {
+            print("Error setting up capture session: \(error.localizedDescription)")
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if let metadata = CMGetAttachment(sampleBuffer, key: kCGImagePropertyExifDictionary, attachmentModeOut: nil) {
+            if let FNumber = metadata["FNumber"] as? Double, let ExposureTime = metadata["ExposureTime"] as? Double, let ISOSpeedRatingsArray = metadata["ISOSpeedRatings"] as? [Double] {
+                
+                let CalibrationConstant = 1000.0
+                
+                let ISOSpeedRatings = ISOSpeedRatingsArray[0]
+                let luminosity: Double = (CalibrationConstant * FNumber * FNumber) / (ExposureTime * ISOSpeedRatings)
+                
+                print(luminosity)
+
+                sendEvent(withName: eventName, body: ["lightValue": luminosity])
+            }
+        }
     }
     
 }
